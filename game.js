aca0f62b0606ff561c6e2651ad19a730c6a29222
Willy Tseng <willytseng@exosite.com>
2018-07-08 14:09:30 +0800

    add table to Game

--- game.js
+++ game.js
@@ -1,5 +1,19 @@
 class Game {
 
+	constructor(size = 3) {
+		if (size < 0) throw Error('invalide table size')
+
+		const outer = new Array(size)
+		for (let i=0; i<size; i++) {
+			outer[i] = new Array(size)
+		}
+
+		this.table = outer
+	}
+
+	clone() {
+		JSON.parse(JSON.stringify(this.table))
+	}
 }
 
 module.exports = Game
--- spec/game.spec.js
+++ spec/game.spec.js
@@ -2,9 +2,42 @@ const Game = require('../game.js')
 
 describe('A Game', () => {
 
-    it('should init', () => {
-        const game = new Game()
-        expect(game).toBeTruthy()
-    })
+	describe('Constructor', ()=> {
+		it('should accept no size on init', () => {
+			const game = new Game()
+			expect(game).toBeTruthy()
+		})
+
+		it('should accept table size >= 3', () => {
+			const game1 = new Game(4)
+			expect(game1).toBeTruthy()
+
+			const game40 = new Game(40)
+			expect(game40).toBeTruthy()
+		})
+
+		it('should have a table with size created', () => {
+			const game4 = new Game(4)
+			expect(game4.table.length).toBe(4)
+			expect(game4.table[0].length).toBe(4)
+
+			const game40 = new Game(40)
+			expect(game40.table.length).toBe(40)
+			expect(game40.table[0].length).toBe(40)
+		})
+	})
+
+	describe('function', () => {
+		let game = null
+
+		beforeAll(() => {
+			game = new Game()
+		})
+
+		it('should return a clone of table', () => {
+			const table = game.clone
+			expect(table).toBeTruthy()
+		})
+	})
 
 })
